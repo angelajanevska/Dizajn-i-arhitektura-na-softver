c790628bafcab5471a00a7e989d7bc7fd7a68160
angelajanevska <80795984+angelajanevska@users.noreply.github.com>
2021-12-06 23:40:32 +0100

    Domashna 2

---
+++ tech prototype/najdiKompjuter/src/main/java/com/example/najdikompjuter/repository/NameSort/NameSort.java
@@ -0,0 +1,12 @@
+package com.example.najdikompjuter.repository.NameSort;
+
+import com.example.najdikompjuter.company.Company;
+
+import java.util.Comparator;
+
+public class NameSort implements Comparator<Company> {
+    @Override
+    public int compare(Company company1, Company company2) {
+        return company1.getName().compareToIgnoreCase(company2.getName());
+    }
+}
