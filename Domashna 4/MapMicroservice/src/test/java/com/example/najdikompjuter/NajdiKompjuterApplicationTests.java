ef425fb6adba392fbcdd4b090c3b00daafe1f708
angelajanevska <80795984+angelajanevska@users.noreply.github.com>
2022-01-31 19:35:04 +0100

    Map Microservice

---
+++ Domashna 4/MapMicroservice/src/test/java/com/example/najdikompjuter/NajdiKompjuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.najdikompjuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NajdiKompjuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
