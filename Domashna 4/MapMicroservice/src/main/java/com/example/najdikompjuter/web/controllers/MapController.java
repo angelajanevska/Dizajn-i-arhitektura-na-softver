27337ce87cc97fa96cda09e07ddbad0704d109a2
angelajanevska <80795984+angelajanevska@users.noreply.github.com>
2022-01-31 23:09:25 +0100

    Computer shops map microservice implementation

---
+++ Domashna 4/MapMicroservice/src/main/java/com/example/najdikompjuter/web/controllers/MapController.java
@@ -0,0 +1,29 @@
+package com.example.najdikompjuter.web.controllers;
+
+import com.example.najdikompjuter.company.Company;
+import com.example.najdikompjuter.services.implementation.CompanyService;
+import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class MapController {
+
+    @Autowired
+    final CompanyService companyService;
+
+    public MapController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/mapa")
+    public List<Company> mapa(Model model){
+        return this.companyService.findAll();
+    }
+}
